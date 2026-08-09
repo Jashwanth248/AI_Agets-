e47bc3f8aefea55bece5d6ddd0062239f291e740
Jashwanth248 <bheemajashwanth9@gmail.com>
2026-08-08 23:07:43 -0700

    Add Terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "project_id" { type = string }
+variable "region" { type = string default = "us-central1" }
+variable "service_name" { type = string default = "llm-auditor-api" }
+variable "image" { type = string }
